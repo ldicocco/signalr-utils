33d6116aec6fa254a572b0e61712b042d1a818df
ldicocco <lu.dicocco@gmail.com>
2013-09-10 01:17:38 +0200

    Start

---
+++ Ldc.SignalR/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ldc.SignalR
+{
+    public class Class1
+    {
+    }
+}
