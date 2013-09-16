8430f03faa2adc248adfd4b06841109c223db22a
ldicocco <lu.dicocco@gmail.com>
2013-09-16 17:58:20 +0200

    Adjusted files

---
+++ Ldc.SignalR/HubExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace Ldc.SignalR
+{
+	public class HubExt : Hub
+	{
+		public Task _result(Guid id, Object result)
+		{
+			return null;
+		}
+	}
+}
