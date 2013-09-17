f8047bddc27173a953765941e3d9c011b912ce7b
ldicocco <lu.dicocco@gmail.com>
2013-09-17 12:11:01 +0200

    Refactoring

---
+++ Ldc.SignalR/Servers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ldc.SignalR
+{
+	class Servers
+	{
+		private static volatile Servers _instance;
+		private static object syncRoot = new Object();
+
+		private readonly IDictionary<string, Server> _servers = null;
+
+		public static Servers Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (syncRoot)
+					{
+						if (_instance == null)
+						{
+							_instance = new Servers();
+						}
+					}
+				}
+
+				return _instance;
+			}
+		}
+
+		private Servers()
+		{
+			_servers = new Dictionary<string, Server>();
+		}
+
+		public void Add(Server server)
+		{
+			lock (syncRoot)
+			{
+				if (!_servers.ContainsKey(server.ConnectionId))
+				{
+					_servers.Add(server.ConnectionId, server);
+				}
+			}
+		}
+
+		public void Remove(string connectionId)
+		{
+			lock (syncRoot)
+			{
+				_servers.Remove(connectionId);
+			}
+		}
+
+		public Server Get(string connectionId)
+		{
+			lock (syncRoot)
+			{
+				Server server = null;
+				_servers.TryGetValue(connectionId, out server);
+				return server;
+			}
+		}
+	}
+}
